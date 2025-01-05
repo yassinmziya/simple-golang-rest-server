02a4b80a833ebb73d99b464a6ab76bc98b82b090
Yassin Mziya <ymziya@ymziya-mn3317.linkedin.biz>
2025-01-04 22:47:50 -0800

    done

---
+++ api.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type api struct {
+	addr string
+}
+
+var users = []User{}
+
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *api) addUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := insertUser(user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func insertUser(u User) error {
+	if u.FirstName == "" {
+		return errors.New("First name is required")
+	}
+	if u.LastName == "" {
+		return errors.New("Last name is required")
+	}
+	for _, user := range users {
+		if user.FirstName == u.FirstName && user.LastName == u.LastName {
+			return errors.New("User already exists")
+		}
+	}
+	users = append(users, u)
+	return nil
+}
